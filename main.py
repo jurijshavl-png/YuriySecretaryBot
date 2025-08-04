d11ca5c94183daa67ba551f7efa9d3db76960e1a
jurijshavl-png <jurijshavl@gmail.com>
2025-08-04 19:08:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from telegram.ext import Application, CommandHandler
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
 from config import BOT_TOKEN
 from commands.reminder import reminder_command
 from commands.fishing import fishing_command
@@ -9,9 +11,20 @@ from commands.construction import construction_command
 from commands.gardening import gardening_command
 from commands.weather import weather_command
 
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ["/weather", "/fishing"],
+        ["/dog", "/car"],
+        ["/documents", "/construction"],
+        ["/gardening", "/napomni"]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text("Выберите команду:", reply_markup=reply_markup)
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
+    application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("napomni", reminder_command))
     application.add_handler(CommandHandler("fishing", fishing_command))
     application.add_handler(CommandHandler("car", car_command))
