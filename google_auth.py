90b022bd5a362351d76e24703e2fb8e1f8cafa25
jurijshavl-png <jurijshavl@gmail.com>
2025-08-04 17:32:59 +0530

    Create google_auth.py

---
+++ google_auth.py
@@ -0,0 +1,22 @@
+# calendar_commands/google_auth.py
+
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+def authenticate_google():
+    creds = None
+    token_path = 'token.json'
+    credentials_path = 'credentials.json'
+
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+        creds = flow.run_local_server(port=8080)
+        with open(token_path, 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
