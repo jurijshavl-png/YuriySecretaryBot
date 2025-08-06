1293460be4d7c6643e3707c81ef670ce3a00836c
jurijshavl-png <jurijshavl@gmail.com>
2025-08-06 13:03:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
+import asyncio
 import logging
-from telegram.ext import Application, CommandHandler
-from commands.fishing import fishing_command
-from commands.weather import weather_command
+from telegram.ext import ApplicationBuilder
+
 from commands.car import car_command
 from commands.dog import dog_command
+from commands.fishing import fishing_command
 from commands.documents import documents_command
-from commands.gardening import gardening_command
 from commands.construction import construction_command
+from commands.gardening import gardening_command
 from commands.reminder import reminder_command
+from weather import weather_command
 
-# Включение логирования
+BOT_TOKEN = "8473269472:AAHoJADfo_3JAC8FfPf2lwQFnWDIxzlzWn8"  
+
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-# Создание приложения с вашим токеном
-application = Application.builder().token("8473269472:AAHoJADfo_3JAC8FfPf2lwQFnWDIxzlzWn8").build()
+async def main():
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Регистрация команд
-application.add_handler(CommandHandler("рыбалка", fishing_command))
-application.add_handler(CommandHandler("погода", weather_command))
-application.add_handler(CommandHandler("авто", car_command))
-application.add_handler(CommandHandler("собака", dog_command))
-application.add_handler(CommandHandler("документы", documents_command))
-application.add_handler(CommandHandler("сад", gardening_command))
-application.add_handler(CommandHandler("стройка", construction_command))
-application.add_handler(CommandHandler("напомни", reminder_command))
+    application.add_handler(car_command)
+    application.add_handler(dog_command)
+    application.add_handler(fishing_command)
+    application.add_handler(documents_command)
+    application.add_handler(construction_command)
+    application.add_handler(gardening_command)
+    application.add_handler(reminder_command)
+    application.add_handler(weather_command)
 
-# Запуск бота
+    await application.initialize()
+    await application.start()
+    print("Бот запущен и работает.")
+    await application.updater.start_polling()
+    await application.updater.idle()
+
 if __name__ == "__main__":
-    application.run_polling()
+    asyncio.run(main())
